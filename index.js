6628e560c541cdc0f2e26bfac7e55f1e084c5e54
Udit <uditmehta100@gmail.com>
2023-09-18 22:29:06 +0530

    team

---
+++ index.js
@@ -0,0 +1,17 @@
+const n=10;
+
+const arr=[];
+
+for(let i=0;i<n;i++)
+{
+  arr[i]=Math.random();
+}
+
+for(let i=0;i<n;i++)
+{
+  const bar=document.createElement("div");
+  bar.style.height=arr[i]*100+"%";
+  bar.style.width="10px";
+  bar.style.backgroundColor="black";
+  container.appendChild(bar);
+}
